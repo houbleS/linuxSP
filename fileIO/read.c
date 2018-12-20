3d2a25efa2a207457069f25dbd5182225170d656
ixkazhenny <ixkazhenny@gmail.com>
2018-12-20 18:57:28 +0900

    add read all byte code in read.c

--- fileIO/read.c
+++ fileIO/read.c
@@ -4,11 +4,22 @@ int main()
 {
   unsigned long word;
   ssize_t nr;
+  ssize_t ret;
 
   /* in 'fd',read some bytes and store on 'word'  */
   nr = read(fd, &word, sizeof(unsigned long));
   if (nr == -1) {
     /* ERRORCODE */
+  }
+
+  while (len != 0 && (ret = read(fd, buf, len) !=0 ) {
+      if (ret == -1) {
+	if (errno == EINTR) {
+	  continue;
+	}
+	perror("read");
+	break;
+      }
   }
   return 0;
 }
